d4182e1f6554524b4a49b852197beaafae618f1a
sandeep-o210130 <psandeepkumark31@gmail.com>
2026-03-19 10:16:08 +0530

    Time: 63 ms (47.97%), Space: 129.2 MB (78.05%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -7,18 +7,10 @@ public:
             if (grid[i][0]=='X') ans[i][0].first++;
             else if(grid[i][0]=='Y') ans[i][0].second++;
             for(int j=1;j<n;j++){
-                if(grid[i][j]=='X'){
-                    ans[i][j].first = ans[i][j-1].first+1;
-                    ans[i][j].second=ans[i][j-1].second;
-                }
-                else if(grid[i][j]=='Y'){
-                    ans[i][j].first = ans[i][j-1].first;
-                    ans[i][j].second=ans[i][j-1].second+1;
-                }
-                else{
-                    ans[i][j].first = ans[i][j-1].first;
-                    ans[i][j].second=ans[i][j-1].second;
-                }
+                ans[i][j].first = ans[i][j-1].first;
+                ans[i][j].second = ans[i][j-1].second;
+                if (grid[i][j]=='X') ans[i][j].first++;
+                else if(grid[i][j]=='Y') ans[i][j].second++;
                 if(i>0){
                     ans[i][j].first = ans[i-1][j].first+ans[i][j].first;
                     ans[i][j].second = ans[i-1][j].second+ans[i][j].second;
