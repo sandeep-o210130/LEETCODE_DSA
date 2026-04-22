8a861f374bb3982d29e5032721f59aa565afa975
sandeep-o210130 <psandeepkumark31@gmail.com>
2026-04-22 09:08:49 +0530

    Time: 8 ms (57.75%), Space: 18.8 MB (43.32%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -3,6 +3,7 @@ public:
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         unordered_map<string,int> mpp;
         vector<string> ans;
+        int n = queries[0].size();
         for(auto i:dictionary) mpp[i]=1;
         int c=0;
         for(int i=0;i<queries.size();i++){
@@ -12,8 +13,9 @@ public:
             }
             for(auto j:dictionary){
                 int c1=0;
-                for(int k=0;k<j.size();k++){
+                for(int k=0;k<n;k++){
                     if(queries[i][k]!=j[k]) c1++;
+                    if(c1>2) break;
                 }
                 if(c1<=2){
                     ans.push_back(queries[i]);
